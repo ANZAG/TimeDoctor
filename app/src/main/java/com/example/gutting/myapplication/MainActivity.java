483bd926e03c387c7799be1cf59c1172b6c030d2
Patrick Gutting <patrick-gutting@t-online.de>
2015-06-26 15:06:00 +0200

    Test

--- app/src/main/java/com/example/gutting/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gutting/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.gutting.myapplication;
 
 import android.app.TabActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
@@ -16,6 +20,8 @@ public class MainActivity extends TabActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_0_start);
 
+        registerReceiver(mybroadcast, new IntentFilter(Intent.ACTION_SCREEN_ON));
+        registerReceiver(mybroadcast, new IntentFilter(Intent.ACTION_SCREEN_OFF));
         /*******************Tabs*****************************/
         //Definieren der Tab Reiter Variablen
         mTabHost = getTabHost();
@@ -60,8 +66,31 @@ public class MainActivity extends TabActivity{
 
         /*******************Tabs*****************************/
            }
+//TEST
+
+    public int on = 0;
+
+    //Create broadcast object
+    BroadcastReceiver mybroadcast = new BroadcastReceiver() {
+        //When Event is published, onReceive method is called
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+            Log.i("[BroadcastReceiver]", "MyReceiver");
+
+            if(intent.getAction().equals(Intent.ACTION_SCREEN_ON)){
+                Log.i("[BroadcastReceiver]", "Screen ON");
+                on++;
+            }
+            else if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+                Log.i("[BroadcastReceiver]", "Screen OFF");
+            }
+
+        }
+    };
 
 
+//TEST
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/example/gutting/myapplication/MainTabActivity.java
+++ app/src/main/java/com/example/gutting/myapplication/MainTabActivity.java
@@ -1,7 +1,10 @@
 package com.example.gutting.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by Gutting on 26.06.2015.
@@ -11,8 +14,12 @@ public class MainTabActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_1_main);
+
+        MainActivity main = new MainActivity();
+        TextView textView = new TextView(this);
+        textView.setText(main.on);
+        setContentView(textView);
     }
 }
